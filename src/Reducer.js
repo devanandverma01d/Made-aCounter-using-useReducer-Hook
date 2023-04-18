3cdb0dd85a963dabfd58b8274aeab2051e7f173b
Devanand Verma <devanandverma01d@gmail.come>
2023-04-18 18:50:11 +0530

    useReducer

--- src/Reducer.js
+++ src/Reducer.js
@@ -11,28 +11,53 @@ const Reducer = () => {
     const reducer = (state,action)=>{
       switch(action)
       {
-        case 'increment':
+        case 'INCREMENT':
           return state+1;
-        case 'decrement':
+        case 'DECREMENT':
           return state-1;
-        case 'reset':
+        case 'RESET':
           return initialState;
+        case 'INCREMENT BY TWO':
+          return state+2;
+        case 'DECREMENT BY TWO':
+          return state-2;
         default:
           return state;
       }
+    
+
+
+    //   if (action==='INCREMENT'){
+    //     return state+1;
+    //   } 
+    //   else if(action==='DECREMENT'){
+    //     return state-1;
+    //   }
+    //   else if(action==='RESET'){
+    //     return initialState;
+    //   }
+    //   else if(action==='INCREMENT BY TWO'){
+    //     return state+2;
+    //   }
+    //   else if(action==='DECREMENT BY TWO'){
+    //     return state-2;
+    //   }
     }
-    const[count,dispatch]=useReducer(reducer,initialState);
+      
+    const[newState,dispatch]=useReducer(reducer,initialState);
     //2.3 Return Statement
   return (
     <div className='main'>
         <h3>Counter - Using useReduder Hook</h3>
         <hr/>
-        <h1>Count = {count}</h1>
-        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('increment')}>Increment</button>
-        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('decrement')}>Decrement</button>
-        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('reset')}>Reset</button>
+        <h1>Count = {newState}</h1>
+        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('INCREMENT')}>Increment</button>
+        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('DECREMENT')}>Decrement</button>
+        <button className='btn btn-danger tbdr' type='button' onClick={()=>dispatch('RESET')}>Reset</button>
+        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('INCREMENT BY TWO')}>Increment by two</button>
+        <button className='btn btn-primary tbdr' type='button' onClick={()=>dispatch('DECREMENT BY TWO')}>Decrement by two</button>
     </div>
   )
 }
 
-export default Reducer
+export default Reducer;
